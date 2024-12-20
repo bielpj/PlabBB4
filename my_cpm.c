f5579314801ff6d72cf0223b4df721d443d2256c
Biel Puga Jodas <bielp@81223290175.local>
2024-12-20 21:24:25 +0100

    mycmp acabat

--- my_cpm.c
+++ my_cpm.c
@@ -1,13 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1
 
 int main(int argc, char *argv[]) {
-  int fd1 = open(argv[1], O_RDONLY);
-  int fd2 = open(argv[2], O_RDONLY);
-  
-  if (fd1 == -1 | fd2 == -1) {
-      perror("Error al obrir els fitxers");
-      exit(-1);
-  }
+    if (argc < 3) {
+        perror("Error: Paràmetres insuficients");
+        exit(-1);
+    }
+
+    int fd1 = open(argv[1], O_RDONLY);
+    if (fd1 < 0) {
+        perror("Error a l'obrir el primer arxiu");
+        exit(-1);
+    }
+
+    int fd2 = open(argv[2], O_RDONLY);
+    if (fd2 < 0) {
+        perror("Error a l'obrir el segon arxiu");
+        exit(-1);
+    }
+
+    char buffer1[BUFFER_SIZE], buffer2[BUFFER_SIZE];
+    int bytesRead1, bytesRead2;
+    int bytePos = 1;
+    int lineNum = 1;
+
+    while ((bytesRead1 = read(fd1, buffer1, BUFFER_SIZE)) > 0 && 
+           (bytesRead2 = read(fd2, buffer2, BUFFER_SIZE)) > 0) {
+        if (buffer1[0] != buffer2[0]) {
+            printf("Diferència trobada al byte %d, línia %d\n", bytePos, lineNum);
+            close(fd1);
+            close(fd2);
+            exit(0);
+        }
+
+        if (buffer1[0] == '\n') {
+            lineNum++;
+        }
+        bytePos++;
+    }
+
+    if (bytesRead1 != bytesRead2) {
+        printf("Diferència trobada: un dels arxius es mes llarg\n");
+        close(fd1);
+        close(fd2);
+        exit(0);
+    }
+
+    close(fd1);
+    close(fd2);
+    return 0;
+}
+
